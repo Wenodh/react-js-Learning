e2274296b49953363081f200653d941362fe8f52
Wenodh <vinodkumara3@gmail.com>
2021-07-11 16:02:15 +0530

    prop with functional component

--- learning-reactjs/src/App.js
+++ learning-reactjs/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Student from './Student';
 import User from './User';
+import React, { useState } from 'react';
 
 function App() {
+    const [name, setName] = useState('wenodh');
     return (
         <div className="App">
+            <Student name={name} />
+            <button
+                onClick={() => {
+                    setName('vinod');
+                }}
+            >
+                Update name
+            </button>
             <User />
         </div>
     );
---
+++ learning-reactjs/src/Student.js
@@ -0,0 +1,4 @@
+function Student(prop) {
+    return <h1>hello {prop.name}</h1>;
+}
+export default Student;
