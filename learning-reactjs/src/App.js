f44b6a1ccd6da98beef66719377976a9382a5748
Wenodh <vinodkumara3@gmail.com>
2021-07-11 16:33:31 +0530

    show hide toggle functions

--- learning-reactjs/src/App.js
+++ learning-reactjs/src/App.js
@@ -8,6 +8,7 @@ function App() {
     const [name, setName] = useState('wenodh');
     const [data, setData] = useState(null);
     const [print, setPrint] = useState(false);
+    const [status, setStatus] = useState(false);
     function getData(val) {
         console.warn(val.target.value);
         setData(val.target.value);
@@ -15,9 +16,15 @@ function App() {
     }
     return (
         <div className="App">
+            {status ? <h1>Hi.....!</h1> : null}
+            <button onClick={() => setStatus(false)}>hide</button>
+            <button onClick={() => setStatus(true)}>show</button>
+            <button onClick={() => setStatus(!status)}>toddle</button>
             {print ? <h1>{data}</h1> : null}
-            <input type="text" onChange={getData} />
-            <button onClick={() => setPrint(true)}>print</button>
+            <div>
+                <input type="text" onChange={getData} />
+                <button onClick={() => setPrint(true)}>print</button>
+            </div>
             <Student name={name} />
             <button
                 onClick={() => {
