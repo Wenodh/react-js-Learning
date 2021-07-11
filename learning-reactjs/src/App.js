7121751f23018d6611f715745f5f8ce5f239d3da
Wenodh <vinodkumara3@gmail.com>
2021-07-11 16:53:25 +0530

    basic form filling

--- learning-reactjs/src/App.js
+++ learning-reactjs/src/App.js
@@ -9,13 +9,51 @@ function App() {
     const [data, setData] = useState(null);
     const [print, setPrint] = useState(false);
     const [status, setStatus] = useState(false);
+    const [input, setInput] = useState(null);
+    const [tnc, setTnc] = useState(false);
+    const [interest, setIntesest] = useState('');
     function getData(val) {
         console.warn(val.target.value);
         setData(val.target.value);
         setPrint(false);
     }
+    function getFormData(e) {
+        console.warn(name, tnc, interest);
+        e.preventDefault();
+    }
     return (
         <div className="App">
+            <h1>Form</h1>
+            <form onSubmit={getFormData}>
+                <input
+                    type="text"
+                    onChange={(e) => {
+                        setInput(e.target.value);
+                    }}
+                />
+                <br />
+                <select
+                    onChange={(e) => {
+                        setIntesest(e.target.value);
+                    }}
+                >
+                    <option>Select Option</option>
+                    <option>marvel</option>
+                    <option>Dc</option>
+                </select>
+                <br />
+
+                <input
+                    type="checkbox"
+                    onChange={(e) => {
+                        setTnc(e.target.checked);
+                    }}
+                />
+                <span>accept terms and conditions</span>
+                <br />
+                <button type="submit">submit</button>
+            </form>
+
             {status ? <h1>Hi.....!</h1> : null}
             <button onClick={() => setStatus(false)}>hide</button>
             <button onClick={() => setStatus(true)}>show</button>
